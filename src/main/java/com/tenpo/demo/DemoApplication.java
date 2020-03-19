898459fb77e0420dc2f953006a8415523130141d
sanjanamurugesan <48680742+sanjanamurugaesan@users.noreply.github.com>
2020-03-19 23:57:45 +0530

    Create DemoApplication.java

---
+++ src/main/java/com/tenpo/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.tenpo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class DemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
