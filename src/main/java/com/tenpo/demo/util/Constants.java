e21a9a730fb73109096a79508b94cf9006fcf517
sanjanamurugesan <48680742+sanjanamurugaesan@users.noreply.github.com>
2020-03-21 23:50:54 +0530

    TCT-01 : New files added

---
+++ src/main/java/com/tenpo/demo/util/Constants.java
@@ -0,0 +1,15 @@
+package com.tenpo.demo.util;
+
+public class Constants {
+    public static final String ADD_SUCCESS = "Added Successfully.";
+    public static final String ERROR = "DB error. Please after some time";
+    public static final String LOGIN = "Login Successfully";
+    public static final String AUTHENTICATION_FAILED ="User authentication failed. Please provide valid login";
+    public static final String USER_NOT_EXISTS = "Sorry, UserID doesn't exist. Please register with your details";
+    public static final String LOGOUT = "Logged out successfully";
+    public static final String SUCCESS = "S";
+    public static final String Y = "Y";
+    public static final String N = "N";
+    public static final String LOGIN_ADVICE = "You are not logged in yet. Please login to see the result.";
+
+}
